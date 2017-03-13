18a717d7580137184188076dbe12567717f1cb1f
David Raymond Christiansen <david@davidchristiansen.dk>
2017-03-13 01:03:20 -0400

    Add zippers as a dependency

--- info.rkt
+++ info.rkt
@@ -1,9 +1,10 @@
 #lang info
 (define collection "abt")
 (define deps '("base"
-               "rackunit-lib"))
+               "rackunit-lib"
+               "zippers"))
 (define build-deps '("scribble-lib" "racket-doc"))
 (define scribblings '(("scribblings/abt.scrbl" ())))
-(define pkg-desc "Description Here")
+(define pkg-desc "Abstract binding trees")
 (define version "0.0")
-(define pkg-authors '(davidc))
+(define pkg-authors '("David Christiansen"))
